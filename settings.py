e5e0ccf01fa2ab78c85de2b7984598cefe07ec85
Ritesh <riteshsaha214@gmail.com>
2025-12-27 16:33:03 +0000

    :recycle: update notion settings

--- settings.py
+++ settings.py
@@ -1,4 +1,5 @@
 from pydantic_settings import BaseSettings, SettingsConfigDict
+import base64
 
 
 class AppSettings(BaseSettings):
@@ -11,11 +12,18 @@ class NotionSettings(BaseSettings):
     client_id: str
     client_secret: str
     auth_url: str
+    redirect_uri: str
 
     model_config = SettingsConfigDict(
         env_prefix="NOTION_",
         env_file=".env",
     )
+
+    @property
+    def encoded_client_id_secret(self) -> str:
+        return base64.b64encode(
+            f"{self.client_id}:{self.client_secret}".encode()
+        ).decode()
 
 
 app_settings = AppSettings()
